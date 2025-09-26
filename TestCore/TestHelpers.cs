2c8e8a6a3cd6d318c070d6b488771c33ad8e46a0
Fedde Talma <ftalma@protonmail.com>
2025-09-26 14:31:54 +0200

    NUnit tests

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -29,16 +29,19 @@ namespace TestCore
 
         //Unhappy flow
         [Test]
-        public void TestPasswordHelperReturnsFalse()
+        public void PasswordHelperShouldReturnFalseWhenPasswordDoesNotMatch()
         {
-            Assert.Pass(); //Zelf uitwerken
+            string password = "wrongPassword";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
+        public void PasswordHelper_ShouldThrow_OnMalformedHash(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            Assert.Throws<FormatException>(() => PasswordHelper.VerifyPassword(password, passwordHash));
         }
     }
 }
